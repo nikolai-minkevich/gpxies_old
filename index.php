82cfd42232f0423c89dd08bc3a3543c64d577f89
nikolai-minkevich <minkevich.nikolai@yandex.ru>
2020-02-16 21:03:51 +0300

    signup.php: Реализовано взаимодействие с БД

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ if (isset($_COOKIE['token'])) {
 /* Проверка авторизации */
 $isAuth = false;
 $mysqli = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldbname);
-$query = "SELECT name, date_reg, token FROM users WHERE token='$cookie_token' ORDER BY date_reg DESC LIMIT 1";
+$query = "SELECT username, date_reg, token FROM users WHERE token='$cookie_token' ORDER BY date_reg DESC LIMIT 1";
 if (mysqli_connect_errno()) {
     printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
     exit;
 }
 if ($result = $mysqli->query($query)) {
     $row = $result->fetch_assoc();
-    if ($row['name'] != '') {
+    if ($row['username'] != '') {
         $isAuth = true;
     } 
     $result->close();
@@ -38,12 +38,13 @@ $mysqli->close();
     <link rel="shortcut icon" href="/gpxies/images/favicon.ico" type="image/x-icon">
 </head>
 
-<body>
-    <?php if ($isAuth) :  ?>
-        <!-- secret content -->
+<?php if ($isAuth) :  ?>
+        <?php 
+        echo "Вы успешно авторизовались";
+        //echo "<script type='text/javascript'> document.location = 'main.php'; </script>"; ?>
     <?php else : ?>
-        <?php echo "<script type='text/javascript'> document.location = 'login.php'; </script>"; ?>
+        <?php //echo "<script type='text/javascript'> document.location = 'login.php'; </script>"; ?>
+        Вы не авторизованы. Для продолжения <a href="login.php">залогиньтесь</a> или <a href="signup.php">зарегистрируйтесь</a>.
     <?php endif ?>
-</body>
 
 </html>
