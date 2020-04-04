28da97d383074c7ee2dc7eeec8f578d1be7e2175
nikolai-minkevich <minkevich.nikolai@yandex.ru>
2020-04-04 15:30:18 +0300

    update some pages

--- create.php
+++ create.php
@@ -16,50 +16,61 @@ include('./_auth.php');
     <title><?php echo ($title); ?></title>
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=Krub:400,600&display=swap" rel="stylesheet">
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-        integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-        crossorigin=""></script>
-    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="        crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.4.0/gpx.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.9/angular.min.js"></script>
-    <script src="../app/app.js"></script>
-    <script src="../app/trackView.js"></script>
+
+
+
 </head>
 
 <body class="create" ng-controller='trackView'>
-    <div class="container">
-        <header class="create-header">
-            <h1 class="title__primary">
-                Создать новый трек
-            </h1>
-        </header>
-    </div>
-    <main class="page-main">
+
+    <?php include('./_menu.php'); ?>
+
+    <?php if ($isAuth) : ?>
+
         <div class="container">
-            <form action="#">
-                <div class="create-form--container">
-                    <select name="sport-choice" id="sport-choice-id">
-                        <option value="1" id="bicycle">Велосипед</option>
-                        <option value="2" id="run">Бег</option>
-                        <option value="3" id="skiing">Лыжи</option>
-                    </select>
-                    <input type="text" class="track-name--input" style="text-indent: 5px;" placeholder="Название трека" required /> 
-                    <div class="private-checkbox--container">
-                        <input type="checkbox" value="1" id="private-checkbox" class="check__input">  <label for="private-checkbox" class="check">
-                            Приватный</label>  
-                    </div>
-                    <button class="button--primary">
-                        Сохранить
-                    </button>
-                </div>
-            </form>
+            <header class="create-header">
+                <h1 class="title__primary">
+                    Создать новый трек
+                </h1>
+            </header>
         </div>
+        <main class="page-main">
+            <div class="container">
+                <form action="#">
+                    <div class="create-form--container">
+                        <select name="sport-choice" id="sport-choice-id">
+                            <option value="1" id="bicycle">Велосипед</option>
+                            <option value="2" id="run">Бег</option>
+                            <option value="3" id="skiing">Лыжи</option>
+                        </select>
+                        <input type="text" class="track-name--input" style="text-indent: 5px;" placeholder="Название трека" required />
+                        <div class="private-checkbox--container">
+                            <input type="checkbox" value="1" id="private-checkbox" class="check__input"> <label for="private-checkbox" class="check">
+                                Приватный</label>
+                        </div>
+                        <button class="button--primary">
+                            Сохранить
+                        </button>
+                    </div>
+                </form>
+            </div>
             <div class="container--max">
                 <div id="mapid"></div>
-                <script src="../js/main.js"></script> 
+                <script src="../js/main.js"></script>
             </div>
-    </main>
-</body>
-</html>
+        </main>
 
+    <?php else : ?>
+
+        <?php include('./_notdenied.php'); ?>
+
+    <?php endif ?>
+
+</body>
+
+</html>
--- list.php
+++ list.php
@@ -19,106 +19,102 @@ include('./_auth.php');
     <link href="https://fonts.googleapis.com/css?family=Krub:400,600&display=swap" rel="stylesheet">
 </head>
 
-<body class="list" >
+<body class="list">
 
     <?php include('./_menu.php'); ?>
 
     <?php if ($isAuth) : ?>
 
-        <!-- Список треков пользователя -->
-
-        <!doctype html>
-
-        <body class="list" ng-controller=''>
-            <header class="list-header">
-                <div class="container container-list">
-                    <h1 class="title__primary">
-                        Список треков
-                    </h1>
-                    <form action="#">
-                        <label for="input-filter">Фильтр
-                            <input id="input-filter" type="text"></label>
-                    </form>
-                </div>
-            </header>
-            <main class="page-main">
+        <header class="list-header">
+            <div class="container container-list">
+                <h1 class="title__primary">
+                    Список треков
+                </h1>
                 <form action="#">
-                    <div class="container container-filter">
-                        <div class="container-filter--checkbox">
-                        </div>
-                        <div class="container-filter--type">
-                            <h3 class="filter--title">Вид спорта</h3>
-                            <select name="sport-choice" id="sport-choice-id">
-                                <option value="1" id="bicycle">Велосипед</option>
-                                <option value="2" id="run">Бег</option>
-                                <option value="3" id="skiing">Лыжи</option>
-                            </select>
-                        </div>
-                        <div class="container-filter--data">
-                            <h3 class="filter--title">Дата</h3>
-                            <div class="container-filter--data__distance">
-                                <label for="input-filter--data__start">c</label>
-                                <input id="input-filter--data__start" type="text" placeholder="дд.мм.гггг">
-                                <label for="input-filter--data__finish">до</label>
-                                <input id="input-filter--data__finish" type="text" placeholder="дд.мм.гггг">
-                            </div>
-                        </div>
-                        <div class="container-filter--name">
-                            <label for="input-filter--name" class="filter--title">Название</label>
-                            <input id="input-filter--name" type="text">
-                        </div>
-                        <div class="container-filter--distance">
-                            <label for="input-filter--distance" class="filter--title">Расстояние</label>
-                            <input id="input-filter--distance" type="text">
-                        </div>
-                        <div class="container-filter--actions">
-                            Действия
-                        </div>
-
-                    </div>
+                    <label for="input-filter">Фильтр
+                        <input id="input-filter" type="text"></label>
                 </form>
-                <div class="container-filter--content">
-                    <div class="container container-filter">
-                        <div class="container-filter--checkbox">
-                            <input type="checkbox">
-                        </div>
-                        <div class="container-filter--type">
-                            Бег
-                        </div>
-                        <div class="container-filter--data">
-                            23.12.1998
-                        </div>
-                        <div class="container-filter--name">
-                            бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю
-                        </div>
-                        <div class="container-filter--distance">
-                            2 км
-                        </div>
-                        <div class="container-filter--actions">
-                            <div class="container-filter--actions__typical">
-                                <a href="#">Ред.</a>
-                                <a href="#">Удал.</a>
-                            </div>
-                            <a href="#" class="button--primary">Скачать GPX</a>
+            </div>
+        </header>
+        <main class="page-main">
+            <form action="#">
+                <div class="container container-filter">
+                    <div class="container-filter--checkbox">
+                    </div>
+                    <div class="container-filter--type">
+                        <h3 class="filter--title">Вид спорта</h3>
+                        <select name="sport-choice" id="sport-choice-id">
+                            <option value="1" id="bicycle">Велосипед</option>
+                            <option value="2" id="run">Бег</option>
+                            <option value="3" id="skiing">Лыжи</option>
+                        </select>
+                    </div>
+                    <div class="container-filter--data">
+                        <h3 class="filter--title">Дата</h3>
+                        <div class="container-filter--data__distance">
+                            <label for="input-filter--data__start">c</label>
+                            <input id="input-filter--data__start" type="text" placeholder="дд.мм.гггг">
+                            <label for="input-filter--data__finish">до</label>
+                            <input id="input-filter--data__finish" type="text" placeholder="дд.мм.гггг">
                         </div>
                     </div>
-
-
+                    <div class="container-filter--name">
+                        <label for="input-filter--name" class="filter--title">Название</label>
+                        <input id="input-filter--name" type="text">
+                    </div>
+                    <div class="container-filter--distance">
+                        <label for="input-filter--distance" class="filter--title">Расстояние</label>
+                        <input id="input-filter--distance" type="text">
+                    </div>
+                    <div class="container-filter--actions">
+                        Действия
+                    </div>
 
                 </div>
+            </form>
+            <div class="container-filter--content">
+                <div class="container container-filter">
+                    <div class="container-filter--checkbox">
+                        <input type="checkbox">
+                    </div>
+                    <div class="container-filter--type">
+                        Бег
+                    </div>
+                    <div class="container-filter--data">
+                        23.12.1998
+                    </div>
+                    <div class="container-filter--name">
+                        бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю бегаю
+                    </div>
+                    <div class="container-filter--distance">
+                        2 км
+                    </div>
+                    <div class="container-filter--actions">
+                        <div class="container-filter--actions__typical">
+                            <a href="#">Ред.</a>
+                            <a href="#">Удал.</a>
+                        </div>
+                        <a href="#" class="button--primary">Скачать GPX</a>
+                    </div>
+                </div>
 
-            </main>
+
+
+            </div>
+
+        </main>
 
 
 
 
 
-        <?php else : ?>
+    <?php else : ?>
 
-            <?php include('./_notdenied.php'); ?>
+        <?php include('./_notdenied.php'); ?>
 
-        <?php endif ?>
-        </body>
+    <?php endif ?>
+
+</body>
 
 
 </html>
